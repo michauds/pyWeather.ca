8efe5125eeffa7c587cac116d7fe92d30acad27a
michauds <michauds90@gmail.com>
2014-02-21 20:54:36 -0500

    Removed old Cities.py dictionary

--- pyweather/Cities.py
+++
@@ -1,8 +0,0 @@
-## Dictionnary associating city names to Environment Canada RSS weather feeds
-url_prefix = "http://www.weatheroffice.gc.ca/rss/city/"
-cities = {
-	"Montreal"		: url_prefix + "qc-147_e.xml",
-	"Longueuil"		: url_prefix + "qc-109_e.xml",
-	"La Prairie"	: url_prefix + "qc-71_e.xml",
-	"Magog"			: url_prefix + "qc-83_e.xml"
-	}
